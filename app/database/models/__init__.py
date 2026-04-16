fc9471327e9973366ded6381b55b1d1474c10921
Ayanokouji <224674406+IATOMYC@users.noreply.github.com>
2026-04-16 22:40:34 +0300

    Create __init__.py

---
+++ app/database/models/__init__.py
@@ -0,0 +1,5 @@
+from app.database.models.user import User
+from app.database.models.feature import Feature
+from app.database.models.audit_log import AuditLog
+
+__all__ = ["User", "Feature", "AuditLog"]
